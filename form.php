032d0159d8eaa8345b312601c8bfad31c438e2fa
x-LALO-x <108778960+x-LALO-x@users.noreply.github.com>
2023-01-11 19:36:47 -0600

    Add files via upload

---
+++ form.php
@@ -0,0 +1,26 @@
+<?php  
+
+$usuario = $_POST['usuario']
+$nombre = $_POST['nombre']
+$apellido = $_POST['apellido']
+$correo = $_POST['email']
+$contra = $_POST['clave1']
+$pais = $_POST['pais']
+$sexo = $_POST['sexo']
+
+$mensaje = "Este mensaje fue enviado por el usuario" . $usuario . "\r\n";
+$mensaje .= "Su nombre es: " . $nombre . " Su apellido es: " . $apellido . "\r\n";
+$mensaje .= "Su correo es: " . $correo . "\r\n";
+$mensaje .= "Su contrasena es: " . $contra . "\r\n";
+$mensaje .= "Su pais es: " . $pais . "\r\n";
+$mensaje .= "Su sexo es: " . $sexo . "\r\n";
+$mensaje .= "Enviado el dia: " . date('d/m/y' , date());
+
+$destinatario = 'jim65eduardo@gmail.com';
+$asunto = 'Este correo fue enviado desde mi pagina web1';
+
+mail($destinatario, $asunto, $mensaje , $header);
+
+header('Location:index.html');
+
+?>
